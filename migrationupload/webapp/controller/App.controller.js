b806daa0ed86c1d9c1f463817c51b889053fea37
Derrick Chuah <derrick.chuah@walldorfconsulting.com>
2025-05-10 15:06:47 +0000

    Implemented routing v2

--- migrationupload/webapp/controller/App.controller.js
+++ migrationupload/webapp/controller/App.controller.js
@@ -1,10 +1,36 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (BaseController) => {
+  "./BaseController",
+  "sap/ui/model/json/JSONModel"
+], (BaseController, JSONModel) => {
   "use strict";
 
   return BaseController.extend("intech.migrationupload.controller.App", {
-      onInit() {
-      }
+    onInit: function () {
+      var oViewModel,
+        fnSetAppNotBusy,
+        iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
+
+      oViewModel = new JSONModel({
+        busy: false,
+        delay: 0,
+        layout: "OneColumn",
+        previousLayout: "",
+        actionButtonsInfo: {
+          midColumn: {
+            fullScreen: false
+          }
+        }
+      });
+      this.getView().setModel(oViewModel, "appView");
+
+      // fnSetAppNotBusy = function () {
+      //   oViewModel.setProperty("/busy", false);
+      //   oViewModel.setProperty("/delay", iOriginalBusyDelay);
+      // };
+      
+      // since then() has no "reject"-path attach to the MetadataFailed-Event to disable the busy indicator in case of an error
+      // this.getOwnerComponent().getModel().metadataLoaded().then(fnSetAppNotBusy);
+      // this.getOwnerComponent().getModel().attachMetadataFailed(fnSetAppNotBusy);
+    }
   });
 });
--- migrationupload/webapp/model/models.js
+++ migrationupload/webapp/model/models.js
@@ -1,20 +1,49 @@
 sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/ui/Device"
-], 
-function (JSONModel, Device) {
-    "use strict";
+],
+    function (JSONModel, Device) {
+        "use strict";
 
-    return {
-        /**
-         * Provides runtime information for the device the UI5 app is running on as a JSONModel.
-         * @returns {sap.ui.model.json.JSONModel} The device model.
-         */
-        createDeviceModel: function () {
-            var oModel = new JSONModel(Device);
-            oModel.setDefaultBindingMode("OneWay");
-            return oModel;
-        }
-    };
-
-});
+        return {
+            /**
+             * Provides runtime information for the device the UI5 app is running on as a JSONModel.
+             * @returns {sap.ui.model.json.JSONModel} The device model.
+             */
+            createDeviceModel: function () {
+                var oModel = new JSONModel(Device);
+                oModel.setDefaultBindingMode("OneWay");
+                return oModel;
+            },
+            /**
+             * Define default content for credit memo model during initial load
+             * @returns {sap.ui.model.json.JSONModel} The credit memo model.
+             */
+            initiateModel: function () {
+                // var s4Base = window.location.hostname.includes("protest") ? "myprod123" :
+                //     window.location.hostname.includes("pro774184b2") ? "my403692" :
+                //         "my403379"; // Dev as default
+                return new JSONModel({
+                    bSelected: false
+                    // legendItems: [
+                    //     {
+                    //         text: this.getResourceBundle().getText("PartialCancel"),
+                    //         color: "#dd6100"
+                    //     },
+                    // ]
+                });
+            },
+            /**
+             * Capture the table columns defined in the view
+             * @returns {Object}  Metadatahelper.
+             */
+            createMetadataHelper: function () {
+                var oTable = this.byId("idDebitMemoTable");
+                return oTable.getColumns().map((oColumn, iIndex) => ({
+                    key: this.getView().getLocalId(oColumn.getId()) || oColumn.getId(),
+                    label: oColumn.getLabel()?.getText() || "",
+                    index: iIndex
+                }));
+            }
+        };
+    });
